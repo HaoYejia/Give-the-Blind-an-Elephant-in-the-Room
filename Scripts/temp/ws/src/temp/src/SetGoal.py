59e22126ed1f9d0ea76fcc9ea302f0a979083177
Haoyejia <haoyejia@outlook.com>
2019-07-29 11:11:20 +0800

    update

--- Scripts/temp/ws/src/temp/src/SetGoal.py
+++ Scripts/temp/ws/src/temp/src/SetGoal.py
@@ -2,7 +2,6 @@
 # -*- coding: UTF-8 -*-
 
 
-
 # ........................................................................................................................................................................................................
 # ........................................................................................................................................................................................................
 # ........................................................................................................................................................................................................
@@ -212,47 +211,44 @@
 # Charles保佑，机器能跑，建图不毛，导航不飘，bug特少
 
 
-
-
 import rospy
-from sensor_msgs.msg import LaserScan
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import PoseStamped
+from std_msgs.msg import String
 
+class SetGoal():
+    def SetGoal(self,msg):
+        if msg.String == self.case0[0]:
+            self.pubmsg.pose.position.x = self.case0[1]
+            self.pubmsg.pose.position.y = self.case0[2]
+            self.pubmsg.pose.position.x = self.case0[3]
+            self.pubmsg.pose.orientation.x = self.case0[4]
+            self.pubmsg.pose.orientation.y = self.case0[5]
+            self.pubmsg.pose.orientation.z = self.case0[6]
+            self.pubmsg.pose.orientation.w = self.case0[7]
+            self.pub.publish(pubmsg)
+        # elif msg.String == self.case1[0]:
 
-
-class ObstacleDetect():
-    def viberate(self,motornum): #viberate which motor
-        
-    
-    def map_vib_abs(self,direction): #absolute mapping, or viberate only one motor 
-        motor_num = 4
-        map_range = 360/motor_num #The estimate mapping range 
-        
-
-    def control(self,msg):
-        for i in range(1,360):
-            if msg.ranges[i]< self.obs_distance_lim: #Detect an obstacle that is too close
-                print('too close at direction %d' %(i))
-                self.map_vib_abs(i) #Mapping the direction to the viberation
-            elif msg.ranges[i] < msg.range_min:
-                print('err at direction %d' %(i))
-                self.pubmsg.linear.x = 1500
-                self.pubmsg.angular.y = 90
-            elif msg.ranges[i] == ' inf':
-                print('err at direction %d' %(i))
-            else:
-                print('fine at direction %d' %(i))
+        # elif msg.String == self.case2[0]:
 
 
     def __init__(self):
-        self.obs_distance_lim = 0.3 #The limit of the obstacle that trigger the alert
-        rospy.init_node('ObsDetect', anonymous=False)
-        rate = rospy.Rate(100)
-        self.pub = rospy.Publisher('car/cmd_vel',Twist, queue_size=10)
-        self.pubmsg = Twist()
-        self.obs = rospy.Subscriber('scan',LaserScan, self.control)
+        rospy.init_node('SetGoal',anonymous=False)
+        self.set = rospy.Subscriber('voice_recon',String, self.SetGoal)
         rospy.spin()
+        self.pub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
+        self.pubmsg = PoseStamped()
+
+        # Use 8 values to store the data of the destination
+        # The first value is name
+        # 2nd to 4th is coordinates x,y,z
+        # The followings are oriantation, x,y,z and w in quardrants  
+        # The transformation of the quardrants and matrix can be done by MATLAB
+        self.case0[8] = ['chufang',0,0,0,0,0,0,0]
+        #self.case1[8] = 0
+        #self.case2[8] = 0
+
+
 
 
 if __name__ == '__main__':
-    whatever = ObstacleDetect()
+    whatever = SetGoal()
